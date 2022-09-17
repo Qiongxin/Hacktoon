80c72ee2808d8d635994650f157b6e6c334ed575
Splitxorpio <mailtoarnavpandey@gmail.com>
2022-09-17 13:04:42 -0700

    V2

--- level.py
+++ level.py
@@ -1,8 +1,22 @@
 import pygame, sys
+from settings import *
+from tile import Tile
+from player import Player
 class Level:
     def __init__(self):
         self.display = pygame.display.get_surface()
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles = pygame.sprite.Group() 
+        self.create_map()
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILE_SIZE
+                y = row_index * TILE_SIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacles])
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites])
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display)
+        self.visible_sprites.update()
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption('')
+        pygame.display.set_caption('Magnolia')
         self.clock = pygame.time.Clock()
-    
+        self.level = Level()
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -18,6 +18,7 @@ class Game:
                     sys.exit()
     
             self.screen.fill('black')
+            self.level.run()
             pygame.display.flip()
             self.clock.tick(TICK_SPEED)
 
